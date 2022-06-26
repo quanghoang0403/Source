16bf90c1c05cf8dd01e81c619dcb2f0a6ffcb8af
quanghoang0403 <quanghoang0403@gmail.com>
2022-06-26 22:35:31 +0700

    commit

--- WebKaraoke/index.js
+++ WebKaraoke/index.js
@@ -1,12 +1,15 @@
 $(document).ready(function (e) {
   $(document).on("click", ".title-region", function () {
-    console.log($(this));
+    $(".region").not($(this).parent(".region")).removeClass("active");
     $(this).parent(".region").toggleClass("active");
+    $(".region").not($(this).parent(".region")).find(".listing").slideUp();
     $(this).parent(".region").find(".listing").slideToggle();
   });
 
   $(document).on("click", ".listing li", function () {
+    $(".listing li").not(this).removeClass("active");
     $(this).toggleClass("active");
+    $(".listing li").not(this).find(".lyrics").slideUp();
     $(this).find(".lyrics").slideToggle();
   });
 });
